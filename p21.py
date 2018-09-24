9bd466f934ae23517c3927883d9c7c32da1b5872
Ashwin <42800493+ashwin015@users.noreply.github.com>
2018-09-24 10:02:25 +0530

    Update p21.py

--- p21.py
+++ p21.py
@@ -1,6 +1,6 @@
-x1,z1=map(int,input("Enter 1st line").split(' '))
-x2,z2=map(int,input("Enter 2nd line").split(' '))
-x3,z3=map(int,input("Enter 3rd line").split(' '))
+x1,z1=map(int,input().split(' '))
+x2,z2=map(int,input().split(' '))
+x3,z3=map(int,input().split(' '))
 if (x1==x2 and x2==x3) or(z1==z2 and z2==z3):
     print("yes")
 else:
