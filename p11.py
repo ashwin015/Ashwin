33ec20f246c3707fb7262bfca50799ca4a4b9077
Ashwin <42800493+ashwin015@users.noreply.github.com>
2018-09-24 09:55:01 +0530

    Delete p11.py

--- p11.py
+++
@@ -1,7 +0,0 @@
-x1,z1=map(int,input("Enter 1st line").split(' '))
-x2,z2=map(int,input("Enter 2nd line").split(' '))
-x3,z3=map(int,input("Enter 3rd line").split(' '))
-if (x1==x2 and x2==x3) or(z1==z2 and z2==z3):
-    print("yes")
-else:
-    print("no")
