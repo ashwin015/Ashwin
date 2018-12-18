100bfcff162460f9ece1ef5d8b5b3fe82fd9411b
Ashwin <42800493+ashwin015@users.noreply.github.com>
2018-12-18 12:35:18 +0530

    Update 73.py

--- 73.py
+++ 73.py
@@ -1,6 +1,6 @@
-as=int(input("Enter Value"))
+s=int(input("Enter Value"))
 d,t=map(int,input("Enter Two values").split(' '))
-if as in range(d+1,t):
+if s in range(d+1,t):
 print("yes")
 else:
 print("no")
